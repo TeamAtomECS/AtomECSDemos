f46bb40396dc0caccb73c9c243f0521893933e19
Elliot Bentine <7041164+ElliotB256@users.noreply.github.com>
2022-04-29 11:07:06 +0100

    Add shadows to example 3d mot

--- examples/3d_mot.rs
+++ examples/3d_mot.rs
@@ -209,8 +209,33 @@ fn setup_camera(
     // camera
     commands.spawn_bundle(camera);
 
-    commands.spawn_bundle(PointLightBundle {
-        transform: Transform::from_xyz(3.0, 8.0, 5.0),
+    // commands.spawn_bundle(PointLightBundle {
+    //     transform: Transform::from_xyz(3.0, 8.0, 5.0),
+    //     ..default()
+    // });
+
+    const HALF_SIZE: f32 = 10.0;
+    commands.spawn_bundle(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: 30000.0,
+            // Configure the projection to better fit the scene
+            shadow_projection: OrthographicProjection {
+                left: -HALF_SIZE,
+                right: HALF_SIZE,
+                bottom: -HALF_SIZE,
+                top: HALF_SIZE,
+                near: -10.0 * HALF_SIZE,
+                far: 10.0 * HALF_SIZE,
+                ..default()
+            },
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0.0, 2.0, 0.0),
+            rotation: Quat::from_rotation_x(-2.0),
+            ..default()
+        },
         ..default()
     });
 }
